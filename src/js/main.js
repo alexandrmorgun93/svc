c2cb5bc0cfd354b105d454ff73b7d3439aa30fea
Kabisimys <alexmorgun93@gmail.com>
2017-11-22 19:01:39 +0200

    f

--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,76 @@
 ;
 (() => {
-    'use strict';
+  'use strict';
 
-    /*variables*/
+  /*variables*/
 
 
-    /*parallax*/
-    const Parallax = require('scroll-parallax');
-    let p = new Parallax('.parallax' ,{
-        intensity :20
-    }).init()
+  /*parallax*/
+  const Parallax = require('scroll-parallax');
+  let p = new Parallax('.parallax', {
+    intensity: 20
+  }).init()
 
-    /*btn-mobile*/
-    /*let btn = document.querySelector('.btn_mobile')
-        btn.addEventListener('click',function () {
-            this.classList.toggle('active');
-        })*/
-   /* let nuv = document.querySelector('.menu_nuv ')
-        nuv.addEventListener('click',function () {
-            nuv.classList.toggle('active');
-        })*/
-    const btn = document.querySelector('.btn_mobile');
-    const nav = document.querySelector('.menu_nuv ');
-        function menuNav(btn,nav) {
-            btn.addEventListener('click',function () {
-                this.classList.toggle('active');
-                nav.classList.toggle('active');
-            })
-        }
+  /*btn-mobile*/
+  /*let btn = document.querySelector('.btn_mobile')
+      btn.addEventListener('click',function () {
+          this.classList.toggle('active');
+      })*/
+  /* let nuv = document.querySelector('.menu_nuv ')
+       nuv.addEventListener('click',function () {
+           nuv.classList.toggle('active');
+       })*/
+  const btn = document.querySelector('.btn_mobile');
+  const nav = document.querySelector('.menu_nuv ');
 
-       menuNav(btn,nav);
+  function menuNav(btn, nav) {
+    btn.addEventListener('click', function() {
+      this.classList.toggle('active');
+      nav.classList.toggle('active');
+    })
+  }
+
+  menuNav(btn, nav);
+  // BEGIN PROJECT SLIDER
+  let slideImg = document.querySelectorAll('.img_slide');
+  let slideText = document.querySelectorAll('.slide_text');
+  let prevSlide = document.querySelector('.prev_slide');
+  let nextSlide = document.querySelector('.next_slide');
+  let yak = 0;
+  // window.onload = function() {
+  //   slide();
+  // }
+  function lastElem(arr, int, boll) {
+    if (boll) {
+      return (arr[int + 1]) ? int + 1 : 0;
+    } else {
+      return (arr[int - 1]) ? int - 1 : arr.length - 1;
+    }
+  }
+
+  function slide(boll) {
+    for (let i = 0; i < slideImg.length; i++) {
+      slideImg[i].classList.remove('active');
+      slideText[i].classList.remove('active');
+    }
+
+
+    yak = lastElem(slideImg, slideText, yak, boll);
+    slideImg[yak].classList.add('active');
+    slideText[yak].classList.add('active');
+  }
+  slide();
+  prevSlide.addEventListener('click', () => {
+    slide(false);
+  })
+
+  nextSlide.addEventListener('click', () => {
+    slide(true);
+  })
+
+
+
+
+
+
 })();
