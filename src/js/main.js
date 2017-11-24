b8802891579d7281d10ba16d6fec171695aa537c
Kabisimys <alexmorgun93@gmail.com>
2017-11-24 18:42:49 +0200

    full

--- src/js/main.js
+++ src/js/main.js
@@ -8,45 +8,51 @@
     /*parallax*/
     const Parallax = require('scroll-parallax');
     let p = new Parallax('.parallax', {
-        intensity: 20
+      intensity: 20
     }).init()
 
 
     const btn = document.querySelector('.btn_mobile');
     const nav = document.querySelector('.menu_nuv ');
+    const language = document.querySelector('.language');
 
     function menuNav(btn, nav) {
-        btn.addEventListener('click', function () {
-            this.classList.toggle('active');
-            nav.classList.toggle('active');
-        })
+      btn.addEventListener('click', function() {
+        this.classList.toggle('active');
+        nav.classList.toggle('active');
+      })
     }
 
     menuNav(btn, nav);
+    language.addEventListener('click', (e) => {
+      if (e.target.parentElement.classList.contains('active')) {
+        e.preventDefault();
+      }
+      language.classList.toggle('active');
+    });
+
+      // BEGIN PROJECT SLIDER
+      let slideImg = document.querySelectorAll('.img_slide');
+      let slideText = document.querySelectorAll('.slide_text');
+      let prevSlide = document.querySelector('.prev_slide');
+      let nextSlide = document.querySelector('.next_slide');
+      let yak = 0;
 
 
-    // BEGIN PROJECT SLIDER
-    let slideImg = document.querySelectorAll('.img_slide');
-    let slideText = document.querySelectorAll('.slide_text');
-    let prevSlide = document.querySelector('.prev_slide');
-    let nextSlide = document.querySelector('.next_slide');
-    let yak = 0;
-
-
-    function lastElem(arr, int, boll) {
+      function lastElem(arr, int, boll) {
         if (boll) {
-            return (arr[int + 1]) ? int + 1 : 0;
+          return (arr[int + 1]) ? int + 1 : 0;
         } else {
-            return (arr[int - 1]) ? int - 1 : arr.length - 1;
+          return (arr[int - 1]) ? int - 1 : arr.length - 1;
         }
-    }
+      }
 
-    function slide(boll) {
+      function slide(boll) {
         for (let i = 0; i < slideImg.length; i++) {
-            slideImg[i].classList.remove('active');
+          slideImg[i].classList.remove('active');
         }
         for (let i = 0; i < slideText.length; i++) {
-            slideText[i].classList.remove('active');
+          slideText[i].classList.remove('active');
         }
         console.log(yak);
 
@@ -55,19 +61,19 @@
 
         yak = lastElem(slideText, yak, boll);
         slideText[yak].classList.add('active');
-    }
+      }
 
-    slide();
+      slide();
 
 
-    nextSlide.addEventListener('click', () => {
+      nextSlide.addEventListener('click', () => {
 
         slide(false);
-    });
+      });
 
 
-    prevSlide.addEventListener('click', () => {
+      prevSlide.addEventListener('click', () => {
         slide(true);
-    })
+      })
 
-})();
+    })();
